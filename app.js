1efe7beec620ca2edc740fa5d6e51a9d5cdc8fc3
Sadet Turhan <msadetturhan1@gmail.com>
2022-11-29 12:29:05 +0300

    some functionality

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 let board = document.querySelector(".board");
-board.style.gridTemplateColumns = 'repeat(16 , 1fr)';
-board.style.gridTemplateRows = 'repeat(16 , 1fr)';
+let squares = document.querySelectorAll("div");
+board.style.gridTemplateColumns = "repeat(16, 1fr)";
+board.style.gridTemplateRows = "repeat(16, 1fr)";
 
-for(let i = 0; i < 256;i++){
+function createBoard(){
+for(let i = 0;i < 256; i++){
   let square = document.createElement("div");
+  square.addEventListener("mouseover",color);
   square.style.backgroundColor = "green";
-  board.insertAdjacentElement("beforeend", square);
+  square.className = "grid";
+  board.insertAdjacentElement("beforeend",square);
+  let resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", reset);
+
+  function reset(){
+    square.style.backgroundColor = "green"
+  };
+}};
+
+createBoard();
+
+function color(){
+  this.style.backgroundColor = "black";
 };
+
+
+
