7d825964c83cc0c282a04e2a7c94e47d7542d4c1
Sadet Turhan <msadetturhan1@gmail.com>
2022-11-29 12:48:38 +0300

    click event added

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ let board = document.querySelector(".board");
 let squares = document.querySelectorAll("div");
 board.style.gridTemplateColumns = "repeat(16, 1fr)";
 board.style.gridTemplateRows = "repeat(16, 1fr)";
+let resetButton = document.querySelector(".reset");
+let blueButton = document.querySelector(".blue");
+let blackButton = document.querySelector(".black");
 
 function createBoard(){
 for(let i = 0;i < 256; i++){
   let square = document.createElement("div");
-  square.addEventListener("mouseover",color);
+  square.addEventListener("click",color);
   square.style.backgroundColor = "green";
   square.className = "grid";
   board.insertAdjacentElement("beforeend",square);
-  let resetButton = document.querySelector(".reset");
-resetButton.addEventListener("click", reset);
+  resetButton.addEventListener("click", reset);
 
   function reset(){
     square.style.backgroundColor = "green"
