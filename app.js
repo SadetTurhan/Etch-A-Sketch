d32df08164f8a82979fe4276d068c14848ca0636
Sadet Turhan <msadetturhan1@gmail.com>
2022-12-24 12:07:09 +0300

    some color changes

--- app.js
+++ app.js
@@ -18,18 +18,36 @@ function createBoard(){
 for(let i = 0;i < boardSize*boardSize; i++){
   let square = document.createElement("div");
   square.addEventListener("click",color);
-  square.style.backgroundColor = "green";
+  square.style.backgroundColor = "darkgrey";
   square.className = "grid";
   blueButton.addEventListener("click",chooseBlue);
   randomButton.addEventListener("click",chooseRandom);
   board.insertAdjacentElement("beforeend",square);
   resetButton.addEventListener("click", reset);
   function reset(){
-    square.style.backgroundColor = "green"
+    square.style.backgroundColor = "darkgrey"
   };
   let boardSize = document.getElementById("sizeInput").value
   sizeButton.addEventListener("click",reset);
   sizeButton.addEventListener("click",createBoard);
+  function color(){
+    if (colorChoice == randomColor){
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;;
+    }
+    else{
+      this.style.backgroundColor = "darkblue";
+    }
+  };
+  
+  function chooseBlue(){
+    colorChoice.pop();
+    colorChoice.push("blue");
+  }
+  
+  function chooseRandom(){
+    colorChoice.pop();
+    colorChoice.push(randomColor);
+  }
   
       
   }};
@@ -38,24 +56,6 @@ for(let i = 0;i < boardSize*boardSize; i++){
 createBoard();
 
 
-function color(){
-  if (colorChoice == randomColor){
-  this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;;
-  }
-  else{
-    this.style.backgroundColor = "blue";
-  }
-};
-
-function chooseBlue(){
-  colorChoice.pop();
-  colorChoice.push("blue");
-}
-
-function chooseRandom(){
-  colorChoice.pop();
-  colorChoice.push(randomColor);
-}
 
 
 
